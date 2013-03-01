b1886d7ea73aab79460f81375d7acf85d394d58c
Takayuki Goto <tak.yuki@gmail.com>
2013-03-01 08:54:48 -0600

    Landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Gaming::Application.routes.draw do
 
   root :to => 'home#index'
 
+
   match '/auth/:provider/callback' => 'sessions#create'
   match '/auth/failure' => 'sessions#failure'
   match '/signout' => 'sessions#destroy', :as => :signout
-  match '/signin' => 'sessions#new', :as => :signout
+  match '/signin' => 'sessions#new', :as => :signin
 end
