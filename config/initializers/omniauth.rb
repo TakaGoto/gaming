bf5179bca8ce023f8a6fc02fd6964fe8b2ff2a05
Takayuki Goto <tak.yuki@gmail.com>
2013-03-26 22:29:07 -0500

    faceb oauth info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '405871032842473', '860e9488ebc9aae395e28754c5d177b7'
+  provider :facebook, '498568113509294', '8fb2d6a1fc453c35842e62c02761dade'
 end
