a7d162d70539b662bf7681990ffb589a0037ffa0
Takayuki Goto <tak.yuki@gmail.com>
2013-04-02 15:59:19 -0500

    fixing js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,4 @@
 //= require twitter/bootstrap
 //= require_tree .
 
-_gaq = _gaq or []
-_gaq.push ["_setAccount", "UA-39685994-1"]
-_gaq.push ["_trackPageview"]
-(->
-  ga = document.createElement("script")
-  ga.type = "text/javascript"
-  ga.async = true
-  ga.src = ((if "https:" is document.location.protocol then "https://ssl" else "http://www")) + ".google-analytics.com/ga.js"
-  s = document.getElementsByTagName("script")[0]
-  s.parentNode.insertBefore ga, s
-)()
+
