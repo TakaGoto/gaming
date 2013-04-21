c590a2415a86011292e65e4d8b637a4f15e2eaa0
Takayuki Goto <tak.yuki@gmail.com>
2013-04-20 21:19:38 -0500

    twitch bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '498568113509294', '8fb2d6a1fc453c35842e62c02761dade'
-  provider :google_twitch, "oaf6abjgztln932floua3dyxqdm6rf7", "sbbqy6rb8nl4u19fqltoftqvy0w959f"
+  provider :twitch_oauth2, "oaf6abjgztln932floua3dyxqdm6rf7", "sbbqy6rb8nl4u19fqltoftqvy0w959f"
 end
